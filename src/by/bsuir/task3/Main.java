76e893d59717db9538601f41c3a172dba1b2e1ab
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-16 11:32:48 +0300

    Add input values checking to Task 3

--- src/by/bsuir/task3/Main.java
+++ src/by/bsuir/task3/Main.java
@@ -9,6 +9,13 @@ public class Main {
     private static final double step = 0.1;
 
     public static void main(String[] args) {
+        if ((domainLeftBorder < domainRightBorder) && (step > 0))
+            performComputing();
+        else
+            System.out.println("Incorrect data!");
+    }
+
+    private static void performComputing() {
         LinkedHashMap<Double, Double> values = TangentFunction.getValues(domainLeftBorder, domainRightBorder, step);
         values.forEach((argument, value) -> System.out.printf("%.5f -> %.5f\n", argument, value));
     }
