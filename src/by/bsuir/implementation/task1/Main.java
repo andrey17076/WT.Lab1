058121f65ede8ec0e7cf61ddca6f657b1a4098b7
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-16 11:00:04 +0300

    Add static import to argument

--- src/by/bsuir/implementation/task1/Main.java
+++ src/by/bsuir/implementation/task1/Main.java
@@ -2,9 +2,10 @@ package by.bsuir.implementation.task1;
 
 public class Main {
 
+    private static final double x = 0;
+    private static final double y = 0;
+
     public static void main(String[] args) {
-        double x = 0;
-        double y = 0;
         System.out.printf("Function value = %.5f", MathFunction.computeValue(x, y));
     }
 }
--- src/by/bsuir/implementation/task3/Main.java
+++ src/by/bsuir/implementation/task3/Main.java
@@ -3,8 +3,13 @@ package by.bsuir.implementation.task3;
 import java.util.LinkedHashMap;
 
 public class Main {
+
+    private static final double domainLeftBorder = -Math.PI;
+    private static final double domainRightBorder = Math.PI;
+    private static final double step = 0.1;
+
     public static void main(String[] args) {
-        LinkedHashMap<Double, Double> values = TangentFunction.getValues(-Math.PI, Math.PI, 0.1);
-        values.forEach( (argument, value) -> System.out.printf("%.5f -> %.5f\n", argument, value));
+        LinkedHashMap<Double, Double> values = TangentFunction.getValues(domainLeftBorder, domainRightBorder, step);
+        values.forEach((argument, value) -> System.out.printf("%.5f -> %.5f\n", argument, value));
     }
 }
